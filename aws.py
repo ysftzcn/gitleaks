26e0a7a2fb4a308b442b2f17b11b1053716a9914
ysftzcn <yusuftezcan@gmail.com>
2023-11-23 16:10:08 +0300

    nactez

--- aws.py
+++ aws.py
@@ -1,3 +1,5 @@
 [435673367886]
-aws_access_key_id = AHKAXSOPPC5HDTTCRD4Z
-aws_secret_access_key = d+qmXdmDssPpwYZbBssljq7Xe7ciNss5+0OAddzs
+aws = AKGAXSOPPC5HDTTCRD4Z
+aws = d+qmXdmDssPpwYZbBssljq7Xe7ciNss5+0OAddzs
+
+by nactez
